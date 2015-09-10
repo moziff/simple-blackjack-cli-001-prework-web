ede7f9d7bc5130c394131de469104510f9873c61
moziff <matteoziff@gmail.com>
2015-09-10 17:12:30 -0400

    woo done with blackjack

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,33 +1,60 @@
+
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  card = rand(1..11)
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
+  
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def get_user_input
   # code #get_user_input here
+  answer = gets.chomp
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  card1=deal_card
+  card2=deal_card
+  sum=card1+card2
+  display_card_total(sum)
+  return sum
+  display_card_total(sum)
 end
 
-def hit?
+def hit?(sum)
   # code hit? here
+  prompt_user
+  answer = get_user_input
+  
+  if answer == "s"
+      return sum
+  elsif answer == "h"
+    new_card=deal_card
+    sum = sum + new_card
+    return sum
+    end
+  
+  
 end
 
 def invalid_command
@@ -40,5 +67,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  sum=initial_round
+  until sum > 22
+  sum=hit?(sum)
+  display_card_total(sum)
+  end
+  end_game(sum)
 end
     
